cc67f4ea66fb8f06a58c6741e16b9efbaa28f255
Christine <guerrerochristinejoy15@gmail.com>
2024-09-14 00:19:49 +0800

    Completed Variables Section

---
+++ app.js
@@ -0,0 +1,88 @@
+//Sample Code
+console.log('Hello World!');
+let name = "CHRISTINE JOY GUERRERO";
+console.log('My name is ' + name);
+
+//Variables
+let myname = ('Dekdek');
+let age = 24;
+console.log('Name:' , myname);
+console.log('Age:' , age , 'years old');
+
+let name_first = 'Mika';
+console.log(name_first);
+
+let flower = 'Rose';
+console.log(flower);
+
+let country = 'Canada';
+console.log(country);
+
+let average = 55;
+console.log(average);
+
+let score = 99;
+console.log(score);
+
+let colors = ['pink', 'green', 'red', 'yellow'];
+console.log(colors);
+
+//Numbers
+let num1 = 10;
+let num2 = 24;
+let sum = num1 + num2;
+console.log('The sum of', num1, 'and', num2, 'is', sum);
+
+const favNumber1 = 15;
+const favNumber2 = 22;
+const product = favNumber1 * favNumber2;
+console.log(product);
+
+const number1 = 200;
+const number2 = 5;
+const quotient = number1 / number2;
+console.log(quotient);
+
+//Booleans
+let a = 30;
+let c = 20;
+let isGreater = a > c;
+let isEqual = a == c;
+console.log(isGreater);
+console.log(isEqual);
+
+let zero = 0;
+let nonZero = 88;
+console.log(Boolean(zero));
+console.log(Boolean(nonZero));
+
+let isActive = true;
+let isOffline = false;
+console.log(isActive);
+console.log(isOffline);
+
+//Comparison Operators
+let x = 100;
+let y = 350;
+let isXGreaterThanY = x > y;
+let isXLessThanY = x < y;
+let isXEqualToY = x == y;
+let isXNotEqualToY = x !== y;
+console.log('Is x greater than y?', isXGreaterThanY);
+console.log('Is x less than y?', isXLessThanY);
+console.log('Is x equal than y?', isXEqualToY);
+console.log('Is x not equal than y?', isXNotEqualToY);
+
+//Strings
+let greeting = ('Good Morning');
+let length = greeting.length;
+console.log(length);
+
+let fruits = ['Apple', 'Banana', 'Cherry'];
+console.log(fruits.length);
+
+let countries = ['Hawaii', 'Canada', 'South Korea', 'Hong Kong'];
+console.log(countries.length);
+
+let animal = ('Zebra');
+console.log(animal.length);
